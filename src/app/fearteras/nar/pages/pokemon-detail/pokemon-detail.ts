2a508bc7277e26e73e1fd01845ce321813321f97
Jose Vanegas <josevanp05@gmail.com>
2025-12-01 18:00:11 -0500

    first commit

--- src/app/fearteras/nar/pages/pokemon-detail/pokemon-detail.ts
+++ src/app/fearteras/nar/pages/pokemon-detail/pokemon-detail.ts
@@ -12,13 +12,13 @@ export class PokemonDetail {
     private route = inject(ActivatedRoute);
   private service = inject(SimpsonSservice);
 
-  personaje = toSignal(
-    this.route.paramMap.pipe(
-      map(params => +params.get('id')!),
-      switchMap(id => this.service.getCharacterById(id))
-    ),
-    { initialValue: null }
-  );
+  //personaje = toSignal(
+    //this.route.paramMap.pipe(
+      //map(params => +params.get('id')!),
+      //switchMap(id => this.service.getCharacterById(id))
+    //),
+  //  { initialValue: null }
+  //);
 }
 function toSignal(arg0: any, arg1: { initialValue: null; }) {
   throw new Error('Function not implemented.');
